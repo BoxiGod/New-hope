e1da027ce26d36b2af6078f0daeaf965c4edd021
BoxiGod <DevirLand@gmail.com>
2019-03-25 22:46:29 +0300

    Add files via upload

--- New-hope.py
+++ New-hope.py
@@ -23,11 +23,10 @@ blocksPerCompetition = 1240;
 yourPrivateKey = ''
 yourAddress = ''
 betBlock = 2 #choose blocks till end when make a bet
-nodes = ['https://nodes.wavesnodes.com', 'https://api.vienna-node.eu', 'http://173.212.243.216:6869', 'http://138.197.155.74:6869']
+nodes = ['http://localhost:6869', 'https://nodes.wavesnodes.com', 'https://api.vienna-node.eu', 'http://173.212.243.216:6869', 'http://138.197.155.74:6869']
 GameAddress = pw.Address(privateKey = 'YnuT613AHFzSX7FQQQEDpa1xy6TYjMaxbuvVKgN87n4')
 yourAccount = pw.Address(privateKey = yourPrivateKey)
 attempts = 10 * len(nodes)
-
 
 def sendData(paymId, winHeight):    #creating data to send
     data = [
@@ -47,12 +46,30 @@ def sendData(paymId, winHeight):    #creating data to send
         "value": winHeight
         },
     ]
-    return GameAddress.dataTransaction(data, signer = yourPrivateKey, txFee = 500000)
+    for attempt in range(attempts):
+        try:
+            return GameAddress.dataTransaction(data, signer = yourPrivateKey, txFee = 500000)
+        except requests.exceptions.RequestException:
+            changeNode()
+            continue
+        else:
+            break
+    else:
+        raise Exception("All attempts failed")
 
 #send Waves, it's easy
 def sendWaves():
-    data = yourAccount.sendWaves(recipient = pw.Address('3P7qcbeYEnD9B7GPHwkhNv2pmDZTiYwVDLw'), amount = 100500000, txFee = 100000)
-    logger.info("Waves sent")
+    for attempt in range(attempts):
+        try:
+            data = yourAccount.sendWaves(recipient = pw.Address('3P7qcbeYEnD9B7GPHwkhNv2pmDZTiYwVDLw'), amount = 100500000, txFee = 100000)
+        except requests.exceptions.RequestException:
+            changeNode()
+            continue
+        else:
+            break
+    else:
+        raise Exception("All attempts failed")
+    logger.info("Waves sent, id = %s", data['id'])
     return data['id']
 
 def withdraw():
@@ -61,52 +78,46 @@ def withdraw():
 
 #Function, which returning current amount of blocks till win
 def blocksToWin():
+    dataTx = getData()
+    for data in dataTx:
+            if data['key'] == 'heightToGetMoney':
+                return data['value'] - getHeight()
+
+def getData():
     numNodes = 0
     for attempt in range(attempts):
         try:
-            dataTx = requests.get(nodes[numNodes] + '/addresses/data/3P7qcbeYEnD9B7GPHwkhNv2pmDZTiYwVDLw').json()
+            return requests.get(nodes[numNodes] + '/addresses/data/3P7qcbeYEnD9B7GPHwkhNv2pmDZTiYwVDLw').json()
         except requests.exceptions.RequestException:
-            logger.info("Problem with %s, going to try next one node", nodes[numNodes])
+            logger.warning("Problem with %s, going to try next one node", nodes[numNodes])
             numNodes = (numNodes + 1) % (len(nodes) - 1)
             continue
         else:
             break
     else:
         raise Exception('All attempts failed')
-    for data in dataTx:
-            if data['key'] == 'heightToGetMoney':
-                return data['value'] - pw.height()
 
 #This function return current potential winner
 def currentWinner():
+    dataTx = getData()
+    for data in dataTx:
+            if data['key'] == 'lastPayment':
+                return getTxInfo(data)['sender']
+
+def getTxInfo(data):    
     numNodes = 0
     for attempt in range(attempts):
         try:
-            dataTx = requests.get(nodes[numNodes] + '/addresses/data/3P7qcbeYEnD9B7GPHwkhNv2pmDZTiYwVDLw').json()
+            url = nodes[numNodes] + "/transactions/info/%s" % data['value']
+            return requests.get(url).json()
         except requests.exceptions.RequestException:
-            logger.info("Problem with %s, going to try next one node", nodes[numNodes])
+            logger.warning("Problem with %s, going to try next one node", nodes[numNodes])
             numNodes = (numNodes + 1) % (len(nodes) - 1)
             continue
         else:
             break
     else:
-        raise Exception('All attempts failed')
-    for data in dataTx:
-            if data['key'] == 'lastPayment':
-                numNodes = 0
-                for attempt in range(attempts):
-                    try:
-                        url = nodes[numNodes] + "/transactions/info/%s" % data['value']
-                        winTx = requests.get(url).json()
-                    except requests.exceptions.RequestException:
-                        logger.info("Problem with %s, going to try next one node", nodes[numNodes])
-                        numNodes = (numNodes + 1) % (len(nodes) - 1)
-                        continue
-                    else:
-                        break
-                else:
-                    raise Exception('All attempts failed')
-                return winTx['sender']
+        raise Exception('All attempts failed')    
 
 def isGame(currHeight, startRound):
     if (currHeight <= (startRound + 1240)) and blocksToWin() > 0:
@@ -116,25 +127,43 @@ def isGame(currHeight, startRound):
         logger.info("Round is over, waiting for next")
         return False
 
+def changeNode():
+    numNodes = 0
+    r = requests.get(nodes[numNodes])
+    status = r.status_code == requests.codes.ok
+    while not status:
+        logger.warning("Node %s is unavaliable, going to try next", nodes[numNodes])
+        numNodes = (numNodes + 1) % (len(nodes) - 1)
+        r = requests.get(nodes[numNodes])
+        status = r.status_code == requests.codes.ok
+    pw.setNode(node=nodes[numNodes])
+    logger.info("Connected to %s", nodes[numNodes])
+
+
+def getHeight():
+    for attempt in range(attempts):
+        try:
+            return pw.height()
+        except requests.exceptions.RequestException:
+            changeNode()
+            continue
+        else:
+            break
+    else:
+        raise Exception("All attempts failed")
 
 #Main, checking conditions to place a bet
 def main():
-    currHeight = pw.height()
+    currHeight = getHeight()
     currRound = (int((currHeight - gameStart) / 1440))
     startRound = gameStart + (currRound * 1440)
     if isGame(currHeight=currHeight, startRound=startRound):
-        if currHeight == (startRound + 1240):
-            paymId = sendWaves()
-            while currentWinner() != yourAddress:
-                logger.info(sendData(paymId = paymId, winHeight = pw.height()+14))
-                time.sleep(0.3)
-            exit()
         if blocksToWin() <= betBlock and currentWinner() != yourAddress:
             paymId = sendWaves()
             while currentWinner() != yourAddress:
-                logger.info(sendData(paymId = paymId, winHeight = pw.height()+14))
-                time.sleep(0.3)
-            time.sleep(180)
+                logger.info(sendData(paymId = paymId, winHeight = getHeight() + 14))
+            logger.info("Bet done, waiting next turn...")
+            time.sleep(360)
         else:
             if currentWinner() == yourAddress:
                 logger.info("You are current potential winner")
@@ -143,25 +172,13 @@ def main():
         if currentWinner() == yourAddress:
             logger.info("You win!")
             withdraw()
-        while not (isGame(currHeight)):
+        while blocksToWin() <= 0:
+            logger.info("Wait for next round")
             time.sleep(1)
 
 def start():
-    numNodes = 0
     while True:
-        r = requests.get(nodes[numNodes])
-        status = r.status_code == requests.codes.ok
-        while not status:
-            logger.info("Node %s is unavaliable, going to try next", nodes[numNodes])
-            numNodes = (numNodes + 1) % (len(nodes) - 1)
-            r = requests.get(nodes[numNodes])
-            status = r.status_code == requests.codes.ok
-            time.sleep(0.1)
-        pw.setNode(node=nodes[numNodes])
-        logger.info("Connected to %s", nodes[numNodes])
         main()
-        start()
-        time.sleep(0.5)
-#sendWaves(pw.height())
+        time.sleep(0.2)
+
 start()
-#print(sendData(paymId="HSRkEHUX4dGmRXscjiy1FrSS4GuXw7YJJ5gqsJPXebQj", winHeight=(pw.height()+14)))
